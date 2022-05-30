e47bb280e3fdb0203a9b5003aa04409df0e84965
Rehab176 <abdosahin40@gmail.com>
2022-05-30 23:08:20 +0200

    Update

---
+++ Sheet_1/Say Hello.java
@@ -0,0 +1,21 @@
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jetly
+ */
+public class Sheet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        String Name = input.next();
+        System.out.println("Hello, "+Name);
+    }
+    
+}
